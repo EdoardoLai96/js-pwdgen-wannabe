d313186f7e757176d5dacf69876083a8322f265c
Edoardo Lai <edoardolai96@gmail.com>
2022-01-19 15:40:22 +0100

    Esercizio terminato

---
+++ main.js
@@ -0,0 +1,16 @@
+
+
+let textName = prompt ("Scrivi il tuo nome");
+
+console.log(textName);
+
+let textLastName = prompt ("Scrivi il tuo cognome");
+
+console.log(textLastName);
+
+let favoriteColor = prompt ("Scrivi il tuo colore preferito");
+
+console.log(favoriteColor);
+
+
+document.getElementById("miocontenitore") . innerHTML="La tua password è :" + textName + textLastName + favoriteColor + "21";
